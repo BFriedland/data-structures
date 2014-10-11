b3b65d381292385ebfb76eed7e80bb1189d0f179
defzzd <friedland.ben@gmail.com>
2014-10-11 03:55:12 -0700

    Finished linked_list.py and updated test_linked_list.py

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -1,4 +1,7 @@
 '''
+
+Contains a LinkedList class for constructing linked lists out of Node objects.
+
 Partly adapted from:
 http://en.literateprograms.org/Singly_linked_list_%28Python%29
 '''
@@ -27,11 +30,11 @@ class LinkedList:
 
         new_node.next_node = self.head
 
-        self.head = new_node
-
-        if self.tail == None:
+        if self.head is None:
 
             self.tail = new_node
+
+        self.head = new_node
 
         self.created_nodes += 1
 
@@ -51,8 +54,13 @@ class LinkedList:
         # with special functions to make every LinkedList function bounce off
         # the floor instead of sending errors due to the use of None.
 
-        # Make sure tail isn't pointing to a "removed" node:
-        if self.head == None:
+        # If the node being removed is the final node,
+        # remember to set self.tail to None.
+        # This behavior will default to making the tail equal to
+        # the head if the snake is decapitated. This will more readily
+        # reveal any possible future errors relating to removing
+        # the head when you're not supposed to.
+        if self.head is None:
             self.tail = None
 
         self.deleted_nodes += 1
@@ -70,7 +78,7 @@ class LinkedList:
         ''' Traverse the list and return the node containing
         the supplied value if present; otherwise, return None. '''
 
-        if self.head == None:
+        if self.head is None:
             return None
 
         else:
@@ -80,30 +88,63 @@ class LinkedList:
 
         ''' Remove a given Node from the list, where ever it might be. '''
 
+        # This operation is unusually complex due to the number of
+        # things that need to be kept track of. The head and the tail are both
+        # special cases, each with their own re-tagging considerations.
+
         # If the list has no nodes, it's still possible for a node
         # reference to continue existing.
-        if self.head == None:
+        # To preempt this, return None if the list is headless.
+        if self.head is None:
+
             return None
 
         else:
+
             # The head always has None for a previous node.
-            node_before_the_node_to_remove, node_to_remove = self.head.search_self_or_next_for_identity_match(None, node)
+            node_before_the_node_to_remove, node_to_remove = \
+                self.head.search_self_or_next_for_identity_match(None, node)
 
-            node_after_the_node_to_remove = node_to_remove.next_node
+            # If the node being removed is neither the head nor the tail,
+            # it needs to bridge the gap in order to be
+            # "removed" from the list.
+            if (node_to_remove != self.tail) and (node_to_remove != self.head):
 
-            # This bridges the list, effectively removing the node_to_remove.
-            node_before_the_node_to_remove.next_node = node_after_the_node_to_remove
+                node_after_the_node_to_remove = node_to_remove.next_node
+
+                # This bridges the list, effectively
+                # removing the node_to_remove.
+                node_before_the_node_to_remove.next_node = \
+                    node_after_the_node_to_remove
+
+            if node_to_remove == self.tail:
+
+                # This sort of operation always feels wrong to type
+                # since it isn't removing anything but the reference,
+                # but that's how this LinkedList works.
+                # It deletes nothing, only swapping references.
+                if node_before_the_node_to_remove is not None:
+                    node_before_the_node_to_remove.next_node = None
+
+                self.tail = node_before_the_node_to_remove
+
+            if node_to_remove == self.head:
+
+                self.head = node_to_remove.next_node
 
         self.deleted_nodes += 1
 
-    def print_list(self):
+    def __str__(self):
+        # pass
+
+        # def print_list(self):
 
         ''' Print the entirety of the list
         represented as a Python tuple literal. '''
 
-        if self.head == None:
+        if self.head is None:
 
-            print("(None)")
+            string_to_return = "()"
 
         else:
 
@@ -111,23 +152,40 @@ class LinkedList:
 
             incrementor_for_node_printing = 0
 
-            print("("),
+            string_to_return = "("
 
-            while 1:
+            while True:
 
-                print("{}, '{}',".format(str(incrementor_for_node_printing), \
-                                          node_to_check.data)),
+                # If there's only one thing in the list, it doesn't need
+                # the leading comma.
+                # Or if the thing we're about to add is the first thing.
+                if incrementor_for_node_printing != 0:
+
+                    string_to_return += ", "
+
+                if isinstance(node_to_check.data, str):
+
+                    # If it's a string, make it pretend to be a string. o_o
+                    string_to_return += "'" + str(node_to_check.data) + "'"
+
+                else:
+
+                    string_to_return += str(node_to_check.data)
 
                 incrementor_for_node_printing += 1
 
-                if node_to_check.next_node != None:
+                # Terminate assembling the string at the tail of the list.
+                if node_to_check != self.tail:
+
                     node_to_check = node_to_check.next_node
 
                 else:
 
-                    print(")")
-
                     break
+
+            string_to_return += ")"
+
+        return string_to_return
 
 
 class Node:
@@ -144,14 +202,14 @@ class Node:
         if self.data == value:
             return self
 
-        elif self.next_node == None:
+        elif self.next_node is None:
             return None
 
         else:
             return self.next_node.search_self_or_next_for_a_value(value)
 
-    def search_self_or_next_for_identity_match(self, \
-            previous_node, supplied_node):
+    def search_self_or_next_for_identity_match(self, previous_node,
+                                               supplied_node):
 
         # This method is the recursive part of LinkedList.remove()
 
@@ -160,17 +218,13 @@ class Node:
 
         # Leaving this in because it's possible a node reference will remain
         # even after it has had its ties deleted.
-        elif self.next_node == None:
+        elif self.next_node is None:
             return previous_node, None
 
         else:
             # This should return the previous_node identity
             # for the last call.
             # It drags up the returned elements from the very bottom.
-            return self.next_node.search_self_or_next_for_identity_match(self, supplied_node)
-
-# For debugging in a REPL
-test_list = LinkedList()
-
-for each_number in range(1, 10):
-    test_list.insert(each_number)
+            return self.next_node \
+                       .search_self_or_next_for_identity_match(
+                           self, supplied_node)
--- linked-list/test_linked_list.py
+++ linked-list/test_linked_list.py
@@ -1,12 +1,81 @@
-import linked_list
+'''
+Test suite for linked_list.py
+
+Partially adapted from:
+https://github.com/linsomniac/python-unittest-skeleton/blob/master/tests/test_skeleton.py
+
+'''
+
 import unittest
+import linked_list
 
-class Case(unittest.TestCase):
-    def test_starting_out(self):
-        self.assertEqual(1, 1)
 
-def main():
-    unittest.main()
+class test_LinkedList(unittest.TestCase):
 
-if __name__ == "__main__":
-    main()
+    def setUp(self):
+
+        # The program needs a list to test with.
+        self.test_list = linked_list.LinkedList()
+
+        for each_number in range(1, 10):
+            self.test_list.insert(each_number)
+
+    def test_LinkedList(self):
+
+        # This feels pretty superfluous due to setUp, which appears to
+        # be intended to come first, but here it is anyways.
+        # For futureproofing, maybe.
+
+        constructor_test_object = linked_list.LinkedList()
+        self.assertTrue(isinstance(constructor_test_object,
+                                   linked_list.LinkedList))
+
+    def test_insert(self):
+
+        # All insert(val) needs to do is successfully create
+        # a node that holds the value.
+        # This will test the Node class's basic functionality as well.
+        insert_test_string = 'insert() test data'
+        self.test_list.insert(insert_test_string)
+        self.assertEqual(self.test_list.head.data, insert_test_string)
+
+    def test_pop(self):
+
+        # This will remove the test_insert() Node and return the value it had.
+        pop_test_string = 'pop() test data'
+        self.test_list.insert(pop_test_string)
+        pop_test_return_data = self.test_list.pop()
+        self.assertEqual(pop_test_return_data, pop_test_string)
+
+    def test_size(self):
+
+        # This will return the size of the list.
+        # Should be 9, ie the range from 1 to the number before 10
+        # (due to range(1, 10) in setUp() in this test suite).
+        self.assertEqual(self.test_list.size(), 9)
+
+    def test_search(self):
+
+        search_test_string = 'search() test data'
+        self.test_list.insert(search_test_string)
+        # Check if the data in the Node returned by search is equal to
+        # the string used to instantiate that Node.
+        self.assertTrue(self.test_list.search(search_test_string).data ==
+                        search_test_string)
+
+    def test_remove(self):
+
+        remove_test_string = 'remove() test data'
+
+        self.test_list.insert(remove_test_string)
+
+        node_to_remove = self.test_list.search(remove_test_string)
+
+        self.test_list.remove(node_to_remove)
+
+        # It will try to find the string and return None.
+
+        self.assertEqual(self.test_list.search(remove_test_string), None)
+
+
+unittest.main()
