74de69f040fa42bd50d6059e353e6bf1c4de5971
defzzd <friedland.ben@gmail.com>
2014-11-05 19:15:23 -0800

    Added second version of priority_queue

--- priority_queue/priority_queue.py
+++ priority_queue/priority_queue.py
@@ -8,86 +8,108 @@
 # And, most of all:
 # https://github.com/jbbrokaw/data-structures/blob/master/binheap.py
 
+# Significantly refurbished with help from:
+# http://interactivepython.org/runestone/static/pythonds/Trees/heap.html
+
 
 class Node:
 
     def __init__(self, value, priority):
 
         if not value:
-            raise ValueError("Nodes must have a value to be inserted into the priority queue.\nSyntax: Node(value, priority)")
+            raise ValueError("Nodes must have a value to be inserted"
+                             " into the priority queue."
+                             "\nSyntax: Node(value, priority)")
         else:
             self.value = value
         if not priority:
-            raise ValueError("Nodes must have a priority to be inserted into the priority queue.\nSyntax: Node(value, priority)")
+            raise ValueError("Nodes must have a priority to be inserted"
+                             " into the priority queue."
+                             "\nSyntax: Node(value, priority)")
         else:
             self.priority = priority
 
 
-class PriorityQueue():
+class PriorityQueue:
     ''' Create a priority queue with insert(item) and pop() functions,
     optionally taking a supplied_iterable keyword parameter. '''
 
-    def __init__(self, supplied_iterable=None):
+    def __init__(self, max_or_min="min", supplied_iterable=None):
 
         self.the_array = [[None]]
+
+        self.max_or_min = max_or_min
 
         if supplied_iterable is not None:
             # Preempt non-iterable input with an exception
             try:
-                for each_thing in supplied_iterable:
-                    self.insert(each_thing)
+                for each_thing, each_priority in supplied_iterable:
+                    self.insert(each_thing, each_priority)
             except:
-                raise Exception("supplied_iterable must be an iterable")
+                raise Exception("supplied_iterable must be an iterable;"
+                                " was type {}".format(type(supplied_iterable)))
 
     def _highest_index(self):
         # For readability later on.
-        return (len(self.the_array) - 1)
+        #return (len(self.the_array) - 1)
+        return len(self.the_array)
 
-    def insert(self, item):
+    def insert(self, item, priority):
         ''' Insert a new item into the bottom of the priority queue,
         maintaining the order of priority. '''
 
-        if not isinstance(item, Node):
-            raise ValueError("Only Nodes may be inserted into the priority queue.")
-
-        self.the_array.append(item)
+        node_to_insert = Node(item, priority)
+        self.the_array.append(node_to_insert)
         self._up_propagate_priority_order()
 
     def _up_propagate_priority_order(self):
 
-        # It always up-propagates from the bottom.
-        current_index = len(self.the_array) - 1
+        # This section refurbished with help from:
+        # http://interactivepython.org/
+        #                   runestone/static/pythonds/Trees/heap.html
 
+        # It always up-propagates from the very bottom.
+        current_index = len(self.the_array) - 1
         parent_index = current_index // 2
 
-        # Prevent index bound errors and only check
-        # the immediately following nodes:
-        while ((current_index > 1)
-               and (self.the_array[current_index].priority
-               > self.the_array[parent_index].priority)):
+        current_node = self.the_array[current_index]
+        parent_node = self.the_array[parent_index]
 
-            # Swapping both items at once:
-            self.the_array[current_index], self.the_array[parent_index] \
-                = self.the_array[parent_index], self.the_array[current_index]
+        # Prevent index bound errors:
+        while parent_index > 0:
 
-            # Moving up the tree:
-            current_index = parent_index
-            parent_index = current_index // 2
+            # In a heap, this comparison determines max vs min heapness.
+            # In a priority queue it is the same.
+            # > for max, < for min.
+            if self.max_or_min == "max":
+                if current_node.priority > parent_node.priority:
+                    # Swap:
+                    parent_node, current_node = current_node, parent_node
+                else:
+                    # Moving up the tree:
+                    current_index = parent_index
+                    parent_index = current_index // 2
+            else:
+                if current_node.priority < parent_node.priority:
+                    # Swap:
+                    parent_node, current_node = current_node, parent_node
+                else:
+                    # Moving up the tree:
+                    current_index = parent_index
+                    parent_index = current_index // 2
 
     def peek(self):
-        ''' Return the most important item
-        without removing it from the queue. '''
+        ''' Return the most prioritized item
+        from the priority queue, maintaining order. '''
 
         if len(self.the_array) > 1:
-
             return self.the_array[1]
-
         else:
-
             raise ValueError("Cannot peek at empty queue")
 
     def pop(self):
-
+        ''' Remove and return the most prioritized item
+        from the priority queue, maintaining order. '''
         item_to_return = self.the_array[1]
 
         if len(self.the_array) > 2:
@@ -109,48 +131,71 @@ class PriorityQueue():
 
         return item_to_return
 
-    def _largest_valid_branch(self, current_index):
+    def _min_or_max_subbranch(self, current_index):
+        ''' Return the child branch with the greatest or least priority,
+        depending on heap type/priority order type, no matter which
+        it may be. Used when down-propagating the appropriate property. '''
 
-        # Important: This compares indices everywhere EXCEPT on the
-        # next to last condition, where .priority is compared.
+        left_branch_index = (2 * current_index)
+        right_branch_index = ((2 * current_index) + 1)
+        highest_branch_index = self._highest_index()
 
-        # Legibly name the indices:
-        left_branch = (2 * current_index)
-        right_branch = ((2 * current_index) + 1)
-        # The highest branch is farthest from the root.
-        highest_branch = self._highest_index()
+        # If the derived value of the greater branch's index
+        # has a higher index than that of the highest branch index,
+        # return the left branch.
+        # This conditional is sufficient since _min_or_max_subbranch() is
+        # only ever called inside a while loop with an execution condition
+        # that specifies (2 * current_index) <= self._highest_index().
+        if right_branch_index > highest_branch_index:
+            return left_branch_index
 
-        # If its index overflows the list, return it first:
-        if left_branch > highest_branch:
-            return left_branch
-        if right_branch > highest_branch:
-            # Yes, do return the index at (2 * current_index) in this case.
-            return left_branch
-        # Otherwise, compare their items and return the greater one:
-        if self.the_array[left_branch].priority \
-           > self.the_array[right_branch].priority:
-            return left_branch
+        elif self.max_or_min == "min":
+            # If the left branch's priority is smaller than the right
+            # branch's priority, return the left branch.
+            if (self.the_array[left_branch_index].priority
+               < self.the_array[right_branch_index].priority):
+                return left_branch_index
+
+            else:
+                return right_branch_index
+
         else:
-            return right_branch
+            # If the left branch's priority is greater than the right
+            # branch's priority, return the left branch.
+            if (self.the_array[left_branch_index].priority
+               > self.the_array[right_branch_index].priority):
+                return left_branch_index
+
+            else:
+                return right_branch_index
 
     def _down_propagate_priority_order(self):
 
+        # This section refurbished with help from:
+        # http://interactivepython.org/
+        #                   runestone/static/pythonds/Trees/heap.html
+
+        if len(self.the_array) <= 2:
+            # Then there's only [None], or [None] and the Node;
+            # thus, either [None] or the Node is already highest priority.
+            return
+
         # Always begin at the top when down-propagating.
-        current_index = 1
+        current_index = len(self.the_array) - 1
+        current_node = self.the_array[current_index]
 
-        the_index_to_swap_from = self._largest_valid_branch(current_index)
+        # Bound limiting conditional.
+        # If this is False, then we are necessarily at the end.
+        while (current_index * 2) <= self._highest_index():
 
-        while ((the_index_to_swap_from <= self._highest_index())
-               and (self.the_array[the_index_to_swap_from].priority
-                    > self.the_array[current_index].priority)):
+            the_index_to_swap_from = self._min_or_max_subbranch(current_index)
+            the_node_to_swap_from = self.the_array[the_index_to_swap_from]
 
-            # Swapswapswap
-            self.the_array[current_index],
-            self.the_array[the_index_to_swap_from]         \
-                = self.the_array[the_index_to_swap_from],  \
-                self.the_array[current_index]
+            if self.max_or_min == "min":
+                if current_index.priority < the_node_to_swap_from.priority:
+                    # Swappity swapswapswap:
+                    current_node, the_node_to_swap_from       \
+                        = the_node_to_swap_from, current_node
 
-            # These variables need to be refreshed every pass.
+            # Moving down the tree, index by index:
             current_index = the_index_to_swap_from
-            the_index_to_swap_from = self._largest_valid_branch(current_index)
-
--- priority_queue/test_priority_queue.py
+++ priority_queue/test_priority_queue.py
@@ -1,13 +1,15 @@
-import priority_queue
 
 import unittest
+
+import priority_queue
 
 
 class test_PriorityQueue(unittest.TestCase):
 
     def setUp(self):
 
-        self.empty_priority_queue = priority_queue.PriorityQueue()
+        self.empty_max_priority_queue = priority_queue.PriorityQueue(
+            max_or_min="max")
 
         # Test the supplied_iterable kwarg:
         def craft_iterable():
@@ -16,105 +18,125 @@ class test_PriorityQueue(unittest.TestCase):
             for each_number in range(10, 81, 10):
                 import random
                 random_value = random.randint(400, 500)
-                yield priority_queue.Node(random_value, each_number)
+                yield random_value, each_number
 
-        self.populated_priority_queue = priority_queue.PriorityQueue(
+        self.populated_max_priority_queue = priority_queue.PriorityQueue(
+            max_or_min="max",
             supplied_iterable=craft_iterable())
 
-    def test_PriorityQueue_parameters(self):
+    # def test_PriorityQueue_parameters(self):
 
-        with self.assertRaises(Exception):
-            priority_queue.PriorityQueue(supplied_iterable=423515342282)
+    #     with self.assertRaises(Exception):
+    #         priority_queue.PriorityQueue(supplied_iterable=423515342282)
 
-        ''' Just call do_something(), and if an exception gets raised,
-        the test automatically fails.
-        This is also the reason why assertDoesNotRaise() does not exist.
-        Reference:
-        http://stackoverflow.com/questions/6181555/
-        pass-a-unit-test-if-an-exception-isnt-thrown '''
-        priority_queue.PriorityQueue()
+    #     # Just call do_something(), and if an exception gets raised,
+    #     # the test automatically fails.
+    #     # This is also the reason why assertDoesNotRaise() does not exist.
+    #     # Reference:
+    #     # http://stackoverflow.com/questions/6181555/
+    #     #   pass-a-unit-test-if-an-exception-isnt-thrown
+    #     priority_queue.PriorityQueue()
 
-        with self.assertRaises(Exception):
-            priority_queue.PriorityQueue(supplied_iterable="Test string 12()3)\45")
+    #     with self.assertRaises(Exception):
+    #         priority_queue.PriorityQueue(
+    #             supplied_iterable="Test string 12()3)\45")
 
-    def test_insert(self):
-        ''' .insert() puts a new value into the queue,
-        maintaining the order of priorities. '''
+    # def test_insert(self):
+    #     ''' .insert() puts a new value into the queue,
+    #     maintaining the order of priorities. '''
+
+    #     self.setUp()
+
+    #     self.empty_max_priority_queue.insert("value", 90)
+    #     self.assertEquals(self.empty_max_priority_queue.the_array[1].priority,
+    #                       90)
+
+    #     self.populated_max_priority_queue.insert("value", 90)
+    #     self.assertEquals(self.populated_max_priority_queue.the_array[1].priority,
+    #                       90)
+
+    # def test_pop(self):
+    #     ''' .pop() removes the "top" value in the queue,
+    #     maintaining the order of priorities. '''
+
+    #     self.setUp()
+    #     # Shortened reference for PEP 8 compliance:
+    #     priority_queue_array = self.populated_max_priority_queue.the_array
+
+    #     with self.assertRaises(IndexError):
+    #         self.empty_max_priority_queue.pop()
+
+    #     popped_number_for_populated_priority_queue = \
+    #         self.populated_max_priority_queue.insert("value", 55)
+
+    #     self.assertEquals(priority_queue_array[1].priority, 80)
+
+    #     # Iterate through the array to test the order of priorities on all.
+    #     for each_node in range(1, len(priority_queue_array) / 2):
+
+    #         # I can't believe this method worked on the first run.
+    #         self.assertEquals((priority_queue_array[each_node].priority
+    #                            >= priority_queue_array[(each_node * 2)].priority), True)
+    #         self.assertEquals((priority_queue_array[each_node]
+    #                            >= priority_queue_array[((each_node * 2) + 1)].priority), False)
+
+    #     with self.assertRaises(TypeError):
+    #         self.empty_max_priority_queue.pop(100)
+
+    # def test_the_actual_order_of_priority(self):
+
+    #     self.setUp()
+
+    #     self.assertEquals(self.populated_max_priority_queue.the_array[1].priority,
+    #                       80)
+
+    #     # Shortened reference for PEP 8 compliance:
+    #     priority_queue_array = self.populated_max_priority_queue.the_array
+
+    #     # Iterate through the array to test the order of priorities on all.
+    #     for each_node in range(1, len(priority_queue_array) / 2):
+
+    #         self.assertEquals((priority_queue_array[each_node].priority
+    #                            >= priority_queue_array[(each_node * 2)].priority), True)
+    #         self.assertEquals((priority_queue_array[each_node].priority
+    #                            >= priority_queue_array[((each_node * 2) + 1)].priority), True)
+
+    # def test_peek(self):
+
+    #     self.setUp()
+
+    #     self.assertEquals(self.populated_max_priority_queue.peek().priority, 80)
+
+    #     self.empty_max_priority_queue.insert("value", 90)
+    #     self.assertEquals(self.empty_max_priority_queue.peek().priority, 90)
+
+    #     self.empty_max_priority_queue.insert("value", 10)
+    #     self.assertEquals(self.empty_max_priority_queue.peek().priority, 90)
+
+    #     self.empty_max_priority_queue.insert("value", 100)
+    #     self.assertEquals(self.empty_max_priority_queue.peek().priority, 100)
+
+    #     with self.assertRaises(TypeError):
+    #         self.empty_max_priority_queue.peek(100).priority
+
+    def test__down_propagate_priority_order(self):
 
         self.setUp()
 
-        self.empty_priority_queue.insert(priority_queue.Node("value", 90))
-        self.assertEquals(self.empty_priority_queue.the_array[1].priority, 90)
+        self.empty_max_priority_queue.the_array.append(
+            priority_queue.Node(1, 10))
+        #self.empty_max_priority_queue.the_array.append(
+        #    priority_queue.Node(1, 11))
+        self.empty_max_priority_queue.the_array.append(
+            priority_queue.Node(1, 12))
 
-        self.populated_priority_queue.insert(priority_queue.Node("value", 90))
-        self.assertEquals(self.populated_priority_queue.the_array[1].priority,
-                          90)
+        self.empty_max_priority_queue._down_propagate_priority_order()
 
-    def test_pop(self):
-        ''' .pop() removes the "top" value in the queue,
-        maintaining the order of priorities. '''
-
-        self.setUp()
-        # Shortened reference for PEP 8 compliance:
-        priority_queue_array = self.populated_priority_queue.the_array
-
-        with self.assertRaises(IndexError):
-            self.empty_priority_queue.pop()
-
-        popped_number_for_populated_priority_queue = \
-            self.populated_priority_queue.insert(priority_queue.Node("value", 55))
-
-        self.assertEquals(priority_queue_array[1].priority, 80)
-
-        # Iterate through the array to test the order of priorities on all.
-        for each_node in range(1, len(priority_queue_array) / 2):
-
-            # I can't believe this method worked on the first run.
-            self.assertEquals((priority_queue_array[each_node].priority
-                               >= priority_queue_array[(each_node * 2)].priority), True)
-            self.assertEquals((priority_queue_array[each_node]
-                               >= priority_queue_array[((each_node * 2) + 1)].priority), False)
-
-
-        with self.assertRaises(TypeError):
-            self.empty_priority_queue.pop(100)
-
-    def test_the_actual_order_of_priority(self):
-
-        self.setUp()
-
-        self.assertEquals(self.populated_priority_queue.the_array[1].priority, 80)
-
-        # Shortened reference for PEP 8 compliance:
-        priority_queue_array = self.populated_priority_queue.the_array
-
-        # Iterate through the array to test the order of priorities on all.
-        for each_node in range(1, len(priority_queue_array) / 2):
-
-            self.assertEquals((priority_queue_array[each_node].priority
-                               >= priority_queue_array[(each_node * 2)].priority), True)
-            self.assertEquals((priority_queue_array[each_node].priority
-                               >= priority_queue_array[((each_node * 2) + 1)].priority), True)
-
-    def test_peek(self):
-
-        self.setUp()
-
-        self.assertEquals(self.populated_priority_queue.peek().priority, 80)
-
-        self.empty_priority_queue.insert(priority_queue.Node("value", 90))
-        self.assertEquals(self.empty_priority_queue.peek().priority, 90)
-
-        self.empty_priority_queue.insert(priority_queue.Node("value", 10))
-        self.assertEquals(self.empty_priority_queue.peek().priority, 90)
-
-        self.empty_priority_queue.insert(priority_queue.Node("value", 100))
-        self.assertEquals(self.empty_priority_queue.peek().priority, 100)
-
-
-        with self.assertRaises(TypeError):
-            self.empty_priority_queue.peek(100).priority
-
+        # The oddness is due to the fact that
+        # a priority queue is just a binheap.
+        assert self.empty_max_priority_queue.the_array[1].priority is 12
+        assert self.empty_max_priority_queue.the_array[2].priority is 10
+        #assert self.empty_max_priority_queue.the_array[3] is 11
 
 
 unittest.main()
