65bf88df2a5712c8dd5f245d2de459535489fded
BFriedland <friedland.ben@gmail.com>
2014-11-18 17:54:54 -0800

    Created skeleton of insertion_sort

---
+++ insertion_sort/insertion_sort.py
@@ -0,0 +1,38 @@
+
+# With thanks to:
+# http://en.wikipedia.org/wiki/Insertion_sort
+# http://www.geekviewpoint.com/python/sorting/insertionsort
+
+
+def insertion_sort(unsorted_list):
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    # "Include an "if __name__ == '__main__':" block at the end of your
+    # module that demonstrates the performance characteristics of this
+    # sort over a variety of lengths of input in both the best and
+    # worst-case scenarios. Executing your module should print informative
+    # output about the performance of your sort to the terminal."
+
+
+    pass
